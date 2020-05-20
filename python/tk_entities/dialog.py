6455345569140cff60af38c4f90c2581e13b3170
Gaetan Guidet <gatgui@gmail.com>
2020-05-20 19:55:07 +0900

    improved filtering options (code / name, substr or regexp)

--- python/tk_entities/dialog.py
+++ python/tk_entities/dialog.py
@@ -12,6 +12,8 @@ import os
 import sys
 import threading
 import shotgun_api3
+import fnmatch
+import re
 
 
 from Qt import QtCore, QtWidgets
@@ -59,13 +61,97 @@ class AppDialog(QtWidgets.QDialog):
 
         ent_list_widget.itemSelectionChanged.connect(self.disp_fields)
 
+        print("Launching Entities Application...")
+        self._entity_by_code = True
+        self._entity_expr = False
+        self._field_by_code = True
+        self._field_expr = False
+        self._init_entities()
+
         # display widgets
         self.disp_entities()
         self.disp_fields()
 
-        print("Launching Entities Application...")
+    def _value_str(self, val, ljoin="\n", kvjoin=" = "):
+        if isinstance(val, (list, set, tuple)):
+            return ljoin.join(map(self._value_str, val))
+        elif isinstance(val, dict):
+            return ljoin.join(map(lambda x: kvjoin.join(map(self._value_str, x)), val.items()))
+        elif isinstance(val, basestring):
+            return val
+        else:
+            return repr(val)
 
-        return
+    def _init_entities(self):
+        self._entities = {}
+        self._name_to_entity = {}
+
+        entities = self._sg.schema_entity_read(self._proj)
+
+        for entity_name, entity_info in entities.iteritems():
+            name = entity_info.get("name", {})
+            if "value" in name:
+                self._name_to_entity[name["value"]] = entity_name
+
+            self._entities[entity_name] = None
+
+    def _get_entity_fields(self, entity_name):
+        if not entity_name in self._entities:
+            # Unknown entity name
+            return ({}, {})
+
+        entity = self._entities[entity_name]
+        if entity is not None:
+            # Entity fields already initialized
+            return entity
+
+        fields = {}
+        name_to_field = {}
+
+        _fields = self._sg.schema_field_read(entity_name, None, self._proj)
+        if not _fields:
+            return ({}, {})
+
+        for field_name, field_info in _fields.iteritems():
+            info = {}
+
+            name = field_info.get("name", {})
+            if "value" in name:
+                _name = name["value"]
+                info["name"] = _name
+                name_to_field[_name] = field_name
+
+            desc = field_info.get("description", {}).get("value", None)
+            if desc is not None:
+                info["description"] = desc
+
+            edit = field_info.get("editable", {}).get("value", None)
+            if edit is not None:
+                info["editable"] = edit
+
+            vis = field_info.get("visible", {}).get("value", None)
+            if vis is not None:
+                info["visible"] = vis
+
+            typ = field_info.get("data_type", {}).get("value", None)
+            if typ is not None:
+                info["type"] = typ
+
+            props = field_info.get("properties", None)
+            if props is not None:
+                defval = props.get("default_value", {}).get("value", None)
+                if defval is not None:
+                    info["default"] = defval
+
+                vtypes = props.get("valid_types", {}).get("value", None)
+                if vtypes is not None:
+                    info["valid_types"] = vtypes
+
+            fields[field_name] = info
+
+        self._entities[entity_name] = (fields, name_to_field)
+
+        return (fields, name_to_field)
 
     def _on_ent_search_edited(self, text):
         """
@@ -73,7 +159,6 @@ class AppDialog(QtWidgets.QDialog):
         """
         self.ent_filter = text
         self.disp_entities()
-        return
 
     def _on_fld_search_edited(self, text):
         """
@@ -81,7 +166,6 @@ class AppDialog(QtWidgets.QDialog):
         """
         self.fld_filter = text
         self.disp_fields()
-        return
 
     def disp_entities(self):
         """
@@ -93,18 +177,33 @@ class AppDialog(QtWidgets.QDialog):
         # clear list
         ent_list_widget.clear()
 
-        entities = self._sg.schema_entity_read(self._proj)
-        entity_names = entities.keys()
+        entity_names = (self._entities if self._entity_by_code else self._name_to_entity).keys()
+
+        _flt = self.ent_filter.strip()
+        if _flt:
+            if self._entity_expr:
+                try:
+                    _exp = re.compile(_flt, re.IGNORECASE)
+                    flt = lambda x: _exp.search(x) is not None
+                except:
+                    print("Invalid expression: '%s'" % _flt)
+                    # Color field in red
+                    return
+                else:
+                    # Don't color field
+                    pass
+            else:
+                _str = _flt.lower()
+                flt = lambda x: _str in x.lower()
+        else:
+            flt = None
 
         for entity_name in sorted(entity_names):
-            if self.ent_filter:
-                if not entity_name.lower().startswith(self.ent_filter.lower()):
-                    continue
+            if flt and not flt(entity_name):
+                continue
             item = QtWidgets.QListWidgetItem(entity_name)
             item.setData(QtCore.Qt.UserRole, item_role)
             ent_list_widget.addItem(item)
-            # print("{} = {}".format(entity_name, entities[entity_name]))
-        return
 
     def disp_fields(self):
         """
@@ -122,19 +221,32 @@ class AppDialog(QtWidgets.QDialog):
             return
 
         entity_name = entity_item.text()
-        fields = self._sg.schema_field_read(entity_name, None, self._proj)
-        field_names = fields.keys()
+        entity_fields, name_to_code = self._get_entity_fields(entity_name)
+        field_names = (entity_fields if self._field_by_code else name_to_code).keys()
 
-        # print '*' * 40
-        # print entity_name.center(40)
-        # print '*' * 40
+        _flt = self.fld_filter.strip()
+        if _flt:
+            if self._field_expr:
+                try:
+                    _exp = re.compile(_flt, re.IGNORECASE)
+                    flt = lambda x: _exp.search(x) is not None
+                except:
+                    print("Invalid expression: '%s'" % _flt)
+                    # Color field in red
+                    return
+                else:
+                    # Don't color field
+                    pass
+            else:
+                _str = _flt.lower()
+                flt = lambda x: _str in x.lower()
+        else:
+            flt = None
+
         for field_name in sorted(field_names):
-            if self.fld_filter:
-                if not field_name.lower().startswith(self.fld_filter.lower()):
-                    continue
+            if flt and not flt(field_name):
+                continue
             item = QtWidgets.QListWidgetItem(field_name)
             item.setData(QtCore.Qt.UserRole, item_role)
             fld_list_widget.addItem(item)
-            # print("{} = {}".format(field_name, fields[field_name]))
-        return
 
